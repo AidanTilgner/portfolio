5d939c274ad4dd2b4579738bfcd376ade340a9ac
Aidan Tilgner <aidantilgner02@gmail.com>
2021-08-29 14:39:42 -0700

    ready for deployment i think
    Signed-off-by: Aidan Tilgner <aidantilgner02@gmail.com>

--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -8,9 +8,6 @@ import './CallToAction.scss'
 
 function CallToAction({ text, link, download }) {
 
-    const [ArrowAnimated, setArrowAnimated] = useState(false)
-    const ArrowRef = useRef(null)
-
     if(link === undefined){
         console.warn(`You need to put a link in CTA ${text}`)
     }
@@ -31,17 +28,24 @@ function CallToAction({ text, link, download }) {
     useEffect(() => {
         let arrows = Array.from(document.getElementsByClassName('cta__arrow'))
         arrows.forEach(arrow => {
-            arrow.style.animationIterationCount = "1"
+            arrow.style.animationIterationCount = 1
         })
     }, [])
 
-    useEffect(() => {
-        if(ArrowAnimated){
+    let animator = (target, animate) => {
+        if(animate){
+            console.log(target, target.children[1])
+            target.children[1]
+            .style
+            .animationIterationCount = 'infinite' 
             console.log('animating')
         }else{
+            target.children[1]
+            .style
+            .animationIterationCount = 1
             console.log('stopping animation')
         }
-    }, [ArrowAnimated])
+    }
 
     return (
         <a 
@@ -50,15 +54,21 @@ function CallToAction({ text, link, download }) {
                 e.preventDefault()
                 linkHandler()
             }}
-            onMouseOver={e => {
+            onMouseEnter={e => {
                 e.preventDefault()
-                setArrowAnimated(true)
+                e.stopPropagation()
+                animator(e.target, true)
+            }}
+            onMouseLeave={e => {
+                e.preventDefault()
+                e.stopPropagation()
+                animator(e.target, false)
             }}
             href={link}
             download={download}
         >
             <p className="cta__text">{text}</p>
-            <div className="cta__arrow" ref={ArrowRef}>
+            <div className="cta__arrow">
                 <svg width="61" height="16" viewBox="0 0 61 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M60.7071 8.70711C61.0976 8.31658 61.0976 7.68342 60.7071 7.29289L54.3431 0.928932C53.9526 0.538408 53.3195 0.538408 52.9289 0.928932C52.5384 1.31946 52.5384 1.95262 52.9289 2.34315L58.5858 8L52.9289 13.6569C52.5384 14.0474 52.5384 14.6805 52.9289 15.0711C53.3195 15.4616 53.9526 15.4616 54.3431 15.0711L60.7071 8.70711ZM0 9H60V7H0V9Z" fill="#2256F2"/>
                 </svg>
